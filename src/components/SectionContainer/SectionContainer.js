c3b3770e9631191815a5266942bff5c5bb200688
Nandini Penumarthy <46973121+vpenumar@users.noreply.github.com>
2020-10-03 18:24:53 -0400

    Add files via upload

---
+++ src/components/SectionContainer/SectionContainer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { About } from './../About/About';
+import { Skills } from './../Skills/Skills';
+import { Projects } from './../Projects/Projects';
+import { Experiences } from '../Experiences/Experiences'
+
+import './section-container.css';
+
+export const SectionContainer = ({ 
+    selectedHeading, 
+    selectedQuote,
+    selectedSection,
+    about,
+    skills,
+    projects,
+    experiences, }) => {
+    return (
+        <div className="section-container">
+            <div className="section-heading-container">
+                <h1 className="main-heading">{selectedHeading}</h1>
+                <p className="main-quote">{selectedQuote}</p>
+            </div>
+            <div className="section-component">
+                {
+                    {
+                        about: <About bio={about.bio} />,
+                        skills: <Skills skills={skills} />,
+                        projects: <Projects projects={projects} />,
+                        experiences: <Experiences experiences={experiences} />,
+                    }[selectedSection]
+                }
+            </div>
+        </div>
+    )
+}
